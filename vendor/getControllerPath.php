b1046c47bc7d98ef87be17b91edfa8cb8170a013
klack710 <willow710kut@gmail.com>
2018-05-07 19:19:34 +0900

    404実装

--- vendor/getControllerPath.php
+++ vendor/getControllerPath.php
@@ -15,12 +15,14 @@ function getControllerPath($uri)
     require_once '../route/route.php';
 
     // routesから、コントローラーのパスを取得する
-    if (array_key_exists($uri, $routes) && isset($routes[$uri][0])) {
+    if (array_key_exists($uri, $routes) && isset($routes[$uri])) {
         $file_full_path = $routes[$uri];
-    } elseif (isset($routes_404[0])) {
+    } elseif (isset($routes_404)) {
+        header("HTTP/1.1 404 Not Found");
         $file_full_path = $routes_404;
     } else {
         //(TODO) 404系エラー
+        header("HTTP/1.1 404 Not Found");
         echo '404 not found';
         exit(1);
     }
